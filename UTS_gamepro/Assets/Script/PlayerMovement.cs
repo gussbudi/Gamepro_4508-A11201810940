2534c60266e229fcca053ab6437a7017d0f5ff4f
novalans1 <novalans47@gmail.com>
2021-11-16 17:28:25 +0700

    Menambahkan commit file

---
+++ UTS_gamepro/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //putar karakter
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(4, 4, 4);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-4, 4, 4);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
+    }
+}
